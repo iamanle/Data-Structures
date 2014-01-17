360410cd6794abb34a15e706f903f9d4b06306bf
An Le <le.quyan@yahoo.com>
2014-01-16 16:07:08 -0800

    nt

--- Assignment/Assignment 1/src/data_structures/Stack.java
+++ Assignment/Assignment 1/src/data_structures/Stack.java
@@ -16,7 +16,7 @@ public class Stack<E> implements Iterable<E>{
 	}
 	
 	//inserts the object obj into the stack
-	//If use addFirst then removeFirst. Use addLast then removeLast. Since addFirst/removeFirst take O(1):
+	//Use addFirst then removeFirst. Use addLast then removeLast. Since addFirst/removeFirst take O(1):
 	public void push(E obj){
 		list.addFirst(obj);
 	} 
